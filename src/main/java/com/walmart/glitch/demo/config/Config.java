19dcb0582c60a04787a72d0df9c5d417aa1e5964
s1b099n <Sheeba.bano@walmartlabs.com>
2020-12-13 16:37:27 +0530

    generate code in given output directory

---
+++ src/main/java/com/walmart/glitch/demo/config/Config.java
@@ -0,0 +1,7 @@
+package com.walmart.glitch.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+}
---
+++ src/main/java/com/walmart/glitch/demo/myapi/UserApiController.java
@@ -0,0 +1,23 @@
+package com.walmart.glitch.demo.myapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-12-13T16:32:25.780+05:30[Asia/Kolkata]")
+
+@Controller
+@RequestMapping("${openapi.reflectoring.base-path:/v2}")
+public class UserApiController implements UserApi {
+
+    private final UserApiDelegate delegate;
+
+    public UserApiController(@org.springframework.beans.factory.annotation.Autowired(required = false) UserApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new UserApiDelegate() {});
+    }
+
+    @Override
+    public UserApiDelegate getDelegate() {
+        return delegate;
+    }
+
+}
